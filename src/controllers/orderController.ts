7936b4e8b6c28066714194fb594d427235d539ce
Luis Pinto <dapin46@hotmail.com>
2025-06-02 18:32:08 -0500

    Controller added for handle requests and responses

---
+++ src/controllers/orderController.ts
@@ -0,0 +1,39 @@
+import type { Request, Response } from 'express';
+import orderService from '../services/orderService';
+
+const processOrder = async (
+  req: Request,
+  res: Response,
+  handleOrder: (orderId: string) => Promise<string>
+) => {
+  try {
+    const { orderId } = req.params;
+    const message = await handleOrder(orderId ?? '');
+    res.status(200).send({ message });
+  } catch (error) {
+    res.status(400).send({ message: (error as Error).message });
+  }
+};
+
+const createOrder = async (req: Request, res: Response) => {
+  processOrder(req, res, orderService.createOrder);
+};
+
+const shipOrder = async (req: Request, res: Response) => {
+  processOrder(req, res, orderService.shipOrder);
+};
+
+const deliverOrder = async (req: Request, res: Response) => {
+  processOrder(req, res, orderService.deliverOrder);
+};
+
+const cancelOrder = async (req: Request, res: Response) => {
+  processOrder(req, res, orderService.cancelOrder);
+};
+
+export default {
+  cancelOrder,
+  createOrder,
+  deliverOrder,
+  shipOrder,
+};
